930f76d85a4122fd226575d1aff905e20eb191d8
yasirarism <55983182+yasirarism@users.noreply.github.com>
2021-05-25 06:57:48 +0700

    Update stream.py

--- WebStreamer/bot/plugins/stream.py
+++ WebStreamer/bot/plugins/stream.py
@@ -53,7 +53,7 @@ async def private_receive_handler(c: Client, m: Message):
             return
     try:
         log_msg = await m.forward(chat_id=Var.BIN_CHANNEL)
-        stream_link = "https://{}/{}".format(Var.FQDN, log_msg.message_id) if Var.ON_HEROKU or Var.NO_PORT else \
+        stream_link = "https://link.yasir.my.id/{}".format(log_msg.message_id) if Var.ON_HEROKU or Var.NO_PORT else \
             "http://{}:{}/{}".format(Var.FQDN,
                                     Var.PORT,
                                     log_msg.message_id)
